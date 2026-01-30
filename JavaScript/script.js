d0d4b1e64e3dc16758bf14ecd751afb90faa40a1
Nada7770 <n98761738@gmail.com>
2026-01-30 22:21:16 +0200

    task_2

---
+++ JavaScript/script.js
@@ -0,0 +1,84 @@
+let x=5;
+let y=10;
+
+
+console.log(x + y);
+console.log(x * y+3);
+console.log(y - x);
+console.log((y + x)*2);
+console.log(y + x*2);
+console.log(y / x);
+console.log(y % x);
+console.log(x ** 2);
+console.log((x-2)*(y+1));
+
+
+
+let a=10;
+let b="Hello";        
+let c=true;
+let d={name: "Backend"};
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+
+let xx=7;
+let yy=10;
+console.log(xx > yy);
+console.log(xx >= yy);
+console.log(xx < yy);
+console.log(xx <= yy);
+console.log(xx == yy);
+console.log(xx === yy);
+console.log(xx != yy);
+console.log(xx !== yy);
+
+
+
+let aa=true;
+let bb=false;
+console.log(aa && bb);
+console.log(aa || bb);
+console.log(!aa);  
+console.log(!bb);   
+console.log(aa && !bb);
+console.log(!aa || bb);
+
+
+
+
+let num=20;
+let str="years old";
+console.log( num + str);
+console.log("age:"+ num);
+
+
+
+
+let n=10;
+console.log(n += 5);
+console.log(n -= 3);
+console.log(n *= 2);
+console.log(n /= 3);
+console.log(n %= 4);
+
+let z=8;
+z++;
+console.log(z);
+z--;
+console.log(z); 
+++z;
+console.log(z);
+--z;
+console.log(z);
+
+
+let w = 4;
+let e = 2;
+let r = "3";
+console.log(w + e + r);
+console.log(w + (e+r));
+console.log((w + e) * Number(r));
+console.log(w * e + Number(r));
