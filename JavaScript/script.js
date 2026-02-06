cdbf7442dcc4a1f4bcf07cc166e897b324fe7faa
Nada Mostafa <n98761738@gmail.com>
2026-02-06 18:28:31 +0200

    task_5-Q3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,46 +1,39 @@
-function addTask() {
-    let input = document.getElementById("taskInput");
-    let taskText = input.value;
+let images = [
+    "img/1004e482acd32b3edd8e03d84bceda64.jpg",
+    "img/17c1be3e57629f292ab3147dd083cc42.jpg",
+    "img/586a4f2c1f2b98a05deccf59b1c1cd6d.jpg",
+    "img/5ea32d8ffe699bbccf4a170cc36a2a3b.jpg",
+    "img/724c5c646e2f685a527f1ca93524701c.jpg",
+    "img/b908e04e804212e8e6da7e4ae2d6c378.jpg",
+    
+];
 
-    if (taskText === "") {
-    alert("Please enter a task");
-    return;
+let currentIndex = 0;
+let img = document.getElementById("sliderImage");
+
+img.src = images[currentIndex];
+
+function nextImage() {
+    currentIndex++;
+
+    if (currentIndex === images.length) {
+    currentIndex = 0;
     }
 
-    let tr = document.createElement("tr");
-    let tdTask = document.createElement("td");
-    tdTask.textContent = taskText;
+    img.src = images[currentIndex];
+}
 
+function prevImage() {
+    currentIndex--;
 
-    let tdStatus = document.createElement("td");
-    tdStatus.textContent = "Pending";
-
-
-    tdTask.onclick = function () {
-    tdTask.classList.toggle("done");
-
-    if (tdStatus.textContent === "Pending") {
-        tdStatus.textContent = "Done";
-    } else {
-        tdStatus.textContent = "Pending";
+    if (currentIndex < 0) {
+    currentIndex = images.length - 1;
     }
-    };
 
+    img.src = images[currentIndex];
+}
 
-    let tdAction = document.createElement("td");
-    let btn = document.createElement("button");
-    btn.textContent = "Delete";
-    btn.onclick = function () {
-    tr.remove();
-    };
-
-    tdAction.appendChild(btn);
-
-    tr.appendChild(tdTask);
-    tr.appendChild(tdStatus);
-    tr.appendChild(tdAction);
-
-    document.getElementById("taskTable").appendChild(tr);
-
-    input.value = "";
+function resetImage() {
+    currentIndex = 0;
+    img.src = images[currentIndex];
 }
